4647a2041a5c7b14262bfb6f0258bf87e90d493b
nikhilpahan <nikhilpahan02@gmail.com>
2024-05-15 15:51:18 +0530

    Quiz questions and answer added

---
+++ script.js
@@ -0,0 +1,40 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            { text: "Sharks", correct: false},
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers: [
+            { text: "Vactican City", correct: true},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Russia", correct: false},
+        ] 
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: false},
+            { text: "Antartica", correct: true},
+        ] 
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false},
+        ] 
+    }
+];
+
+
